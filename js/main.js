a266c5c15aa6092732b80e771d1809020135c4ce
Bryan Davila <bdavila90@gmail.com>
2015-07-14 22:56:27 -0400

    ajax and display local xml file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
 var NewsBox = React.createClass({
+  getInitialState : function () {
+    return {data: []};
+  },
+  componentDidMount: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType:"xml",
+      //headers: {'Access-Control-Allow-Origin': '*',
+      //          'Access-Control-Allow-Methods': 'GET'},
+      crossDomain: false,
+      //cache:false,
+      success: function(data) {
+        stories = [];
+        //console.log(data);
+        //xml = $.parseXML(data);
+        list = $.xml2json(data);
+        list.channel.item.forEach( function(ele) {
+          //console.log(ele.title);
+          stories.push({
+            title: ele.title,
+            author: ele.author,
+            story: ele.description,
+            image: ele.content.url,
+            link: ele.link
+          });
+        });
+        console.log(stories);
+        //list = $(data).find('item')
+        //console.log($(list[0]).children('title').text());
+        this.setState({data:stories});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url,status,err.toString());
+      }.bind(this)
+    });
+  },
   render: function () {
     return (
       <div>
-        <NewsList />
-        <FeedForm />
+        <h1>Top Stories</h1>
+        <NewsList data={this.state.data}/>
       </div>
     );
   }
@@ -11,9 +47,33 @@ var NewsBox = React.createClass({
 
 var NewsList = React.createClass({
   render: function () {
+    var itemNodes = this.props.data.map(function (item){
+      return (
+        <Item author={item.author}
+              title={item.title}
+              image={item.image}
+              link={item.link}>
+          {item.story}
+        </Item>
+      );
+    });
     return (
       <div>
-        Hello, NewsList!
+        {itemNodes}
+      </div>
+    );
+  }
+});
+
+var Item = React.createClass({
+  render: function () {
+    var rawMarkup = this.props.children.toString();
+    return (
+      <div>
+        <h2><a href={this.props.link}>{this.props.title}</a></h2>
+        <p className="lead">{this.props.author}</p>
+        <img src={this.props.image} alt=""/>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
@@ -30,6 +90,6 @@ var FeedForm = React.createClass({
 });
 
 React.render(
-  <NewsBox />,
+  <NewsBox url='feed1.xml' />,
   document.getElementById('content')
 );
